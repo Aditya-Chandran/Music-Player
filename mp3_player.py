02048fe7896d051410565fa824858ab9530920d3
Aditya Chandran <106558927+Aditya-Chandran@users.noreply.github.com>
2022-06-25 23:50:12 +0530

    made window

---
+++ mp3_player.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import *
+from turtle import color
+
+mp=Tk()
+mp.iconbitmap(r'rickroll.ico')
+mp.title("ANA Music Player")
+mp.config(bg='#0F111A')
+mp.geometry("960x540")
+mp.resizable(False,False)
+
+mp.mainloop()
