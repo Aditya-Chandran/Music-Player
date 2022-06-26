c87343e941d53dbaa864cddaa7d5c37a241d887d
Aditya Chandran <106558927+Aditya-Chandran@users.noreply.github.com>
2022-06-26 14:48:02 +0530

    Added buttons

--- mp3_player.py
+++ mp3_player.py
@@ -9,4 +9,16 @@ mp.config(bg='#0F111A')
 mp.geometry("960x540")
 mp.resizable(False,False)
 
+previous_button=PhotoImage(file="previous.png")
+previous=Button(mp,image=previous_button,bd=0,bg='#0F111A',activebackground='#0F111A').place(x=90,y=320)
+
+play_button=PhotoImage(file="play.png")
+play=Button(mp,image=play_button,bd=0,bg='#0F111A',activebackground='#0F111A').place(x=210,y=320)
+
+pause_button=PhotoImage(file="pause.png")
+pause=Button(mp,image=pause_button,bd=0,bg='#0F111A',activebackground='#0F111A').place(x=320,y=320)
+
+next_button=PhotoImage(file="next.png")
+next=Button(mp,image=next_button,bd=0,bg='#0F111A',activebackground='#0F111A').place(x=440,y=320)
+
 mp.mainloop()
