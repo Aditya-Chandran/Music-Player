3ad606bbc2a6bde20503d26011081db3878c26a7
Aditya Chandran <106558927+Aditya-Chandran@users.noreply.github.com>
2022-06-26 23:16:09 +0530

    Added frame for playlist and title bar
    I am going crazy at this point. Things that should work normally aren't working. Not getting solutions in google or yt too.

--- mp3_player.py
+++ mp3_player.py
@@ -1,7 +1,17 @@
 import tkinter as tk
 from tkinter import *
 from turtle import color
+from tkinter import filedialog
+from turtle import color
+import os
 
+def open_folder():
+    path=filedialog.askdirectory()
+    if path:
+        os.chdir(path)
+        songs=os.listdir(path)
+        print(songs)
+    
 mp=Tk()
 mp.iconbitmap(r'rickroll.ico')
 mp.title("ANA Music Player")
@@ -20,5 +30,14 @@ pause=Button(mp,image=pause_button,bd=0,bg='#0F111A',activebackground='#0F111A')
 
 next_button=PhotoImage(file="next.png")
 next=Button(mp,image=next_button,bd=0,bg='#0F111A',activebackground='#0F111A').place(x=440,y=320)
+
+title_bar=Label(mp,bg='#040508',height='2',width='400').pack(side=TOP)
+add_song=Button(mp,text='Add Songs',font=('Segoe',14,'bold'),fg='white',bg='#040508',bd=0,activebackground='#212942',activeforeground='#818182',command=open_folder).place(x=10,y=2)
+
+music_frame=Label(mp,height='15',width='66').place(x=100,y=70)
+menu=Frame(mp,bd=1,relief=RIDGE,height='230.5',width='450',bg='#8182FF').place(x=100,y=70)
+
+playlist=Listbox(music_frame,width=66,font=('Segoe',10),fg='white',bg='black',selectbackground='#2E3596',bd=0).pack(side=LEFT,fill=BOTH)
+scroll=Scrollbar(music_frame,orient='vertical',command=playlist).pack(side=RIGHT,fill=Y)
 
 mp.mainloop()
